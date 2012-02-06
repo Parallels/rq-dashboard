47899fcabd2b90cb34c826e485dffe803fe5ee15
Vincent Driessen <vincent@3rdcloud.com>
2012-02-06 15:22:31 +0100

    Simplify URL schemes.

--- rq_dashboard/frontend/views.py
+++ rq_dashboard/frontend/views.py
@@ -6,11 +6,12 @@ from .models import all_queues, all_workers
 
 app = Blueprint('frontend', __name__)
 
-@app.route('/')
-def overview():
+@app.route('/', defaults={'queue_name': 'default'})
+@app.route('/<queue_name>')
+def overview(queue_name):
     return render_template('frontend/overview.html',
             workers=all_workers(),
-            queue=Queue(),
+            queue=Queue(queue_name),
             queues=all_queues())
 
 @app.route('/stats')
