461786613bf46af4ee21b0e68d3fdb6f48f73aed
Vincent Driessen <vincent@3rdcloud.com>
2012-02-14 21:40:01 +0100

    Directly show any failed jobs, if present.
    This fixes #14.

--- rq_dashboard/frontend/views.py
+++ rq_dashboard/frontend/views.py
@@ -6,10 +6,20 @@ from .models import all_queues, all_workers
 
 app = Blueprint('frontend', __name__)
 
-@app.route('/', defaults={'queue_name': 'default'})
+
+@app.route('/', defaults={'queue_name': None})
 @app.route('/<queue_name>')
 def overview(queue_name):
+    if queue_name is None:
+        # Show the failed queue by default if it contains any jobs
+        failed = Queue('failed')
+        if not failed.is_empty():
+            queue = failed
+        else:
+            queue = Queue()
+    else:
+        queue = Queue(queue_name)
     return render_template('frontend/dashboard.html',
             workers=all_workers(),
-            queue=Queue(queue_name),
+            queue=queue,
             queues=all_queues())
