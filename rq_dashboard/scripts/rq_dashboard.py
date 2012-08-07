85e1d1222b741b6a5ad18bf8896fa3a8b757c0da
Vincent Driessen <vincent@3rdcloud.com>
2012-08-07 21:28:59 +0200

    Fine-tune the cmdline options a bit and show usage when superfluous args are passed to it.

--- rq_dashboard/scripts/rq_dashboard.py
+++ rq_dashboard/scripts/rq_dashboard.py
@@ -1,31 +1,42 @@
 #!/usr/bin/env python
+import sys
 import optparse
 from ..app import app
 
 
 def main():
     parser = optparse.OptionParser("usage: %prog [options]")
-    parser.add_option('-l', '--listen', dest='host',
-                      metavar='IP', help='IP to listen on')
+    parser.add_option('-b', '--bind', dest='bind_addr',
+                      metavar='ADDR',
+                      help='IP addr or hostname to bind to')
     parser.add_option('-p', '--port', dest='port', type='int',
-                      metavar='PORT', help='port to listen on')
-    parser.add_option('-s', '--redis-server', dest='redis_host',
-                      metavar='IP', help='IP of redis server')
-    parser.add_option('--redis-port', dest='redis_port', type='int',
-                      metavar='PORT', help='port of redis server')
+                      metavar='PORT',
+                      help='port to bind to')
+    parser.add_option('-H', '--redis-host', dest='redis_host',
+                      metavar='ADDR',
+                      help='IP addr or hostname of Redis server')
+    parser.add_option('-P', '--redis-port', dest='redis_port', type='int',
+                      metavar='PORT',
+                      help='port of Redis server')
     (options, args) = parser.parse_args()
 
-    # populate app.config from options, defaulting to app.config's original
+    # Populate app.config from options, defaulting to app.config's original
     # values, if specified, finally defaulting to something sensible.
-    app.config['HOST'] = options.host or app.config.get('HOST', '0.0.0.0')
+    app.config['BIND_ADDR'] = options.bind_addr or app.config.get('BIND_ADDR', '0.0.0.0')
     app.config['PORT'] = options.port or app.config.get('PORT', 9181)
 
-    # override app.config from options if specified.  Otherwise leave untouched,
+    # Override app.config from options if specified.  Otherwise leave untouched,
     # so the client can use its own defauls if app.config has no values.
-    if options.redis_host: app.config['REDIS_HOST'] = options.redis_host
-    if options.redis_port: app.config['REDIS_PORT'] = options.redis_port
+    if options.redis_host:
+        app.config['REDIS_HOST'] = options.redis_host
+    if options.redis_port:
+        app.config['REDIS_PORT'] = options.redis_port
 
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+    if len(args) > 0:
+        parser.print_help()
+        sys.exit(2)
+
+    app.run(host=app.config['BIND_ADDR'], port=app.config['PORT'])
 
 if __name__ == '__main__':
     main()
