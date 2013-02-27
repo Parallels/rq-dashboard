df0c0e0fd7f6e2fa2cd47ed8968d5db87e52495e
Vincent Driessen <vincent@3rdcloud.com>
2013-02-27 21:29:50 +0100

    Stick to the basics.

--- rq_dashboard/__init__.py
+++ rq_dashboard/__init__.py
@@ -3,28 +3,17 @@ from .dashboard import dashboard
 
 
 class RQDashboard(object):
-    def __init__(self, app=None, url_prefix='/rq', **kwargs):
+    def __init__(self, app=None, url_prefix='/rq'):
         self.url_prefix = url_prefix
         if app is not None:
-            self.init_app(app, **kwargs)
+            self.app = app
+            self.init_app(app)
         else:
             self.app = None
 
-    def init_app(self, app, **kwargs):
-        """Initializes the RQ-Dashboard for the specified application.
-
-        :param app: The application.
-        """
-
-        if not hasattr(app, 'extensions'):
-            app.extensions = {}
-
+    def init_app(self, app):
+        """Initializes the RQ-Dashboard for the specified application."""
         if self.app is not None:
             raise Exception('RQ-dashboard is already associated with an application.')
 
-        self.app = app
-        app.extensions['rq-dashboard'] = self
-
         app.register_blueprint(dashboard, url_prefix=self.url_prefix)
-
-
