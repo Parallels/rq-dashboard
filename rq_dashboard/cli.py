649610d69245172c0ecbb32802497f9ed1503643
voith <voithjm1@gmail.com>
2017-05-18 00:49:02 +0530

    added configuration for import path

--- rq_dashboard/cli.py
+++ rq_dashboard/cli.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import importlib
 import os
+import sys
 
 import click
 from flask import Flask, Response, request
@@ -90,11 +91,13 @@ def make_flask_app(config, username, password, url_prefix):
 @click.option(
     '--interval', default=None, type=int,
     help='Refresh interval in ms')
+@click.option('--path', default='.',
+              help='Specify the import path.')
 def run(
         bind, port, url_prefix, username, password,
         config,
         redis_host, redis_port, redis_password, redis_database, redis_url,
-        interval):
+        interval, path):
     """Run the RQ Dashboard Flask server.
 
     All configuration can be set on the command line or through environment
@@ -106,6 +109,9 @@ def run(
     RQ_DASHBOARD_SETTINGS environment variable.
 
     """
+    if path:
+        sys.path = path.split(':') + sys.path
+
     click.echo('RQ Dashboard version {0}'.format(VERSION))
     app = make_flask_app(config, username, password, url_prefix)
     if redis_url:
