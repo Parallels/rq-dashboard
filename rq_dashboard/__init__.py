8a40d18ebf36e00967c8a4a9265801a487606a04
Dharmendra Verma <dkverma.vit@gmail.com>
2017-06-15 17:40:52 +0530

    Create __init__.py

--- rq_dashboard/__init__.py
+++ rq_dashboard/__init__.py
@@ -1,3 +1,12 @@
+from os.path import exists
+
+from dotenv import load_dotenv
+
+dotenv_path = '/etc/environment'
+if exists(dotenv_path):
+    print("loading environment variables from /etc/environment file..")
+    load_dotenv(dotenv_path)
+
 # flake8: noqa
 from . import default_settings
 from .web import blueprint
