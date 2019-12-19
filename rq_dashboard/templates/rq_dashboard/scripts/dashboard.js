cdd9a5b915387ade97f39206fa0645e170c56d4e
Praden Florian <florian.praden@quicksign.com>
2019-12-19 17:08:23 +0100

    Use a CSRF token if needed

--- rq_dashboard/templates/rq_dashboard/scripts/dashboard.js
+++ rq_dashboard/templates/rq_dashboard/scripts/dashboard.js
@@ -150,6 +150,29 @@ var modalConfirm = function(action, cb) {
 })($);
 
 (function ($) {
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    var CSRF = getCookie("_csrf_token");
+    if (CSRF != ""){
+        $.ajaxSetup({
+            headers: {
+                'X-CSRFToken': CSRF
+            }
+        });
+    }
     $('#autorefresh-switch').change(function() {
         if ($('#autorefresh-switch').prop('checked')){
             AUTOREFRESH_FLAG = 1;
